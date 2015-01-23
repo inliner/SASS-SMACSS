c6a4f20b9ce06910a45ce3c0701155b5c7e686f5
Andrey <andrey.artemenko@gmail.com>
2015-01-23 21:29:51 +0200

    rp
    Make relative paths work

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "../"
+http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
@@ -11,7 +11,7 @@ fonts_dir = "fonts"
 output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
